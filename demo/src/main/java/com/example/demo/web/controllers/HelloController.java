1766b247ca2ee2199533766ae346b05b6a63fe9c
张国延 <410115424@qq.com>
2018-03-10 17:49:53 +0800

    init

---
+++ demo/src/main/java/com/example/demo/web/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.web.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String get() {
+        return "Hello World";
+    }
+}
